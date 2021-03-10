ef638c0c75b471bd025c99ea4940e199920bbc9b
Tyler Erickson <me@tylererickson.com>
2021-03-10 12:27:38 -0800

    Fixed merge errors

--- custom/languages/odin/ext/function_indexer.cpp
+++ custom/languages/odin/ext/function_indexer.cpp
@@ -1,3 +1,4 @@
+
 #ifdef EXT_FUNCTION_INDEX
 function Function_Index *odin_parse_function__findexer(Application_Links *app, Code_Index_Note *note, Arena *arena)
 {
@@ -14,34 +15,33 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
         case TokenOdinKind_LiteralString:
         idx++;
         goto skip_to_params;
-
+        
         default:
         break;
     }
-
+    
     Code_Index_Nest *nest = code_index_get_nest(note->file, tokens.tokens[idx].pos+1);
     if (!nest || nest->kind != CodeIndexNest_Paren)
         return 0;
-
+    
     Range_i64 param_range = Ii64(nest->open.max, nest->close.min);
-
+    
     Function_Index *index = push_array_zero(arena, Function_Index, 1);
     index->note = note;
     index->name = note->text;
     index->parameters = {0};
-
+    
     idx = token_index_from_pos(&tokens, param_range.min);
-
+    
     String_Const_u8 param_string = push_buffer_range(app, arena, buffer, param_range);
     Function_Parameter *param = 0;
-
+    
     while (tokens.tokens[idx].pos < param_range.max)
     {
         String_Const_u8 prefix = {};
         String_Const_u8 type = {};
         String_Const_u8 postfix = {};
         String_Const_u8 name = {};
-<<<<<<< HEAD
         String_Const_u8 default_value = {};
         
         b32 is_specialization = false;
@@ -52,12 +52,6 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
                tokens.tokens[idx].kind == TokenBaseKind_Comment ||
                tokens.tokens[idx].sub_kind == TokenOdinKind_using) idx++;
         
-=======
-
-        while (tokens.tokens[idx].kind == TokenBaseKind_Whitespace ||
-               tokens.tokens[idx].kind == TokenBaseKind_Comment) idx++;
-
->>>>>>> aff74bf50925b6b07a3ad1da08f901fb306ff56c
         //// PARAM NAME
         if (tokens.tokens[idx].sub_kind == TokenOdinKind_Identifier || odin_is_builtin_proc(&tokens.tokens[idx]))
         {
@@ -65,10 +59,10 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
             name.size = tokens.tokens[idx].size;
             idx++;
         }
-
+        
         while (tokens.tokens[idx].kind == TokenBaseKind_Whitespace ||
                tokens.tokens[idx].kind == TokenBaseKind_Comment) idx++;
-
+        
         if (tokens.tokens[idx].sub_kind != TokenOdinKind_Colon)
         {
             type = name;
@@ -79,7 +73,7 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
         {
             idx++;
         }
-
+        
         //// TYPE PREFIX
         type_prefix:
         switch (tokens.tokens[idx].sub_kind)
@@ -87,22 +81,22 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
             case TokenOdinKind_Whitespace:
             idx++;
             goto type_prefix;
-
+            
             case TokenOdinKind_Carrot:
             if (prefix.str == 0)
                 prefix.str = &param_string.str[tokens.tokens[idx].pos - param_range.min];
             idx++;
             goto type_prefix;
-
+            
             case TokenOdinKind_BrackOp:
             {
                 if (prefix.str == 0)
                     prefix.str = &param_string.str[tokens.tokens[idx].pos - param_range.min];
-                Code_Index_Nest *_nest = code_index_get_nest(note->file, tokens.tokens[idx].pos);
-                idx = token_index_from_pos(&tokens, _nest->close.max);
+                Code_Index_Nest *nest = code_index_get_nest(note->file, tokens.tokens[idx].pos);
+                idx = token_index_from_pos(&tokens, nest->close.max);
                 goto type_prefix;
             }
-
+            
             default:
             if (prefix.str != 0)
                 prefix.size = &param_string.str[Ii64(&tokens.tokens[idx-1]).max-param_range.min] - prefix.str;
@@ -110,9 +104,9 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
         }
         while (tokens.tokens[idx].kind == TokenBaseKind_Whitespace ||
                tokens.tokens[idx].kind == TokenBaseKind_Comment) idx++;
-
+        
         //// TYPE NAME
-        /* type_name: */
+        type_name:
         switch (tokens.tokens[idx].sub_kind)
         {
             case TokenOdinKind_Identifier:
@@ -120,7 +114,7 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
             type.size = tokens.tokens[idx].size;
             idx++;
             break;
-
+            
             default:
             if (odin_is_builtin_type(&tokens.tokens[idx]))
             {
@@ -130,21 +124,20 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
             }
             break;
         }
-
+        
         //// TYPE POSTFIX
         type_postfix:
         switch (tokens.tokens[idx].sub_kind)
         {
-
+            
             case TokenOdinKind_ParenOp:
             {
                 if (postfix.str == 0)
                     postfix.str = &param_string.str[tokens.tokens[idx].pos - param_range.min];
-                Code_Index_Nest *_nest = code_index_get_nest(note->file, tokens.tokens[idx].pos);
-                idx = token_index_from_pos(&tokens, _nest->close.max);
+                Code_Index_Nest *nest = code_index_get_nest(note->file, tokens.tokens[idx].pos);
+                idx = token_index_from_pos(&tokens, nest->close.max);
                 goto type_postfix;
             }
-<<<<<<< HEAD
             
             case TokenOdinKind_Div:
             {
@@ -159,16 +152,12 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
                 goto type_prefix;
             }
             
-=======
-
->>>>>>> aff74bf50925b6b07a3ad1da08f901fb306ff56c
             default:
             if (postfix.str != 0)
                 postfix.size = &param_string.str[Ii64(&tokens.tokens[idx-1]).max-param_range.min] - postfix.str;
             break;
         }
         postfix = string_skip_chop_whitespace(postfix);
-<<<<<<< HEAD
         
         if (is_specialization)
         {
@@ -217,9 +206,6 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
         }
         default_value = string_skip_chop_whitespace(default_value);
         
-=======
-
->>>>>>> aff74bf50925b6b07a3ad1da08f901fb306ff56c
         //// SKIP TO COMMA or PAREN
         skip_rest:
         switch (tokens.tokens[idx].sub_kind)
@@ -228,22 +214,22 @@ function Function_Index *odin_parse_function__findexer(Application_Links *app, C
             case TokenOdinKind_ParenCl:
             idx++;
             break;
-
+            
             default:
             idx++;
             goto skip_rest;
         }
-
+        
         param = push_array_zero(arena, Function_Parameter, 1);
-        if (name.data) param->name = push_string_copy(arena, name);
-        if (prefix.data) param->prefix = push_string_copy(arena, prefix);
-        if (postfix.data) param->postfix = push_string_copy(arena, postfix);
-        if (type.data) param->type = push_string_copy(arena, type);
-        if (default_value.data) param->default_value = push_string_copy(arena, default_value);
+        if (name.str) param->name = push_string_copy(arena, name);
+        if (prefix.str) param->prefix = push_string_copy(arena, prefix);
+        if (postfix.str) param->postfix = push_string_copy(arena, postfix);
+        if (type.str) param->type = push_string_copy(arena, type);
+        if (default_value.str) param->default_value = push_string_copy(arena, default_value);
         sll_queue_push(index->parameters.first, index->parameters.last, param);
         print_message(app, default_value);
     }
-
+    
     return index;
 }
 
@@ -267,16 +253,16 @@ function List_String_Const_u8 odin_parameter_strings(Function_Index *index, Aren
                           string_expand(param->default_value)
                           );
     }
-
+    
     return param_strings;
 }
 #undef string_expand_nonnull
 static Language_Function_Indexer odin_function_indexer =
 {
-    /* .lang = */ &language_def_odin,
-    /* .parse_function = */ odin_parse_function__findexer,
-    /* .parameter_strings = */ odin_parameter_strings,
-    /* .delims = */ {
+    .lang = &language_def_odin,
+    .parse_function = odin_parse_function__findexer,
+    .parameter_strings = odin_parameter_strings,
+    .delims {
         TokenOdinKind_ParenOp, SCu8("("),
         TokenOdinKind_ParenCl, SCu8(")"),
         TokenOdinKind_Comma, SCu8(",")
