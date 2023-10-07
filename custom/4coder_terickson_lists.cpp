55ad2fbc11a1a84df248cad69877e317a13b2550
Tyler Erickson <me@tylererickson.com>
2023-10-07 14:36:17 -0700

    Added new lister functionality

--- custom/4coder_terickson_lists.cpp
+++ custom/4coder_terickson_lists.cpp
@@ -1,108 +1,146 @@
+function b32
+tc_buffer_is_in_project(Application_Links *app, Buffer_ID buffer)
+{
+	Scratch_Block scratch(app);
+	Variable_Handle prj_var = vars_read_key(vars_get_root(), vars_save_string_lit("prj_config"));
+	if (vars_is_nil(prj_var)) return false;
+	String8 prj_dir = prj_path_from_project(scratch, prj_var);
+	String8 buffer_name = push_buffer_file_name(app, scratch, buffer);
+	buffer_name = string_replace(scratch, buffer_name, SCu8("\\"), SCu8("/"), StringFill_NoTerminate);
+	return buffer_name.size != string_find_first(buffer_name, prj_dir, StringMatch_Exact);
+}
+
 function void
 tc_generate_all_buffers_list__output_buffer(Application_Links *app, Lister *lister,
-                                            Buffer_ID buffer){
-    Dirty_State dirty = buffer_get_dirty_state(app, buffer);
-    String_Const_u8 status = {};
-    switch (dirty){
-        case DirtyState_UnsavedChanges:  status = string_u8_litexpr("*"); break;
-        case DirtyState_UnloadedChanges: status = string_u8_litexpr("!"); break;
-        case DirtyState_UnsavedChangesAndUnloadedChanges: status = string_u8_litexpr("*!"); break;
-    }
-    
-    Scratch_Block scratch(app, lister->arena);
-    Variable_Handle prj_var = vars_read_key(vars_get_root(), vars_save_string_lit("prj_config"));
-    String_Const_u8 buffer_name = {};
-    if (!vars_is_nil(prj_var))
-    {
-        String8 prj_dir = prj_path_from_project(scratch, prj_var);
-        buffer_name = push_buffer_file_name(app, scratch, buffer);
-        buffer_name = string_replace(scratch, buffer_name, prj_dir, SCu8("./"), StringFill_NoTerminate);
-    }
-    if (buffer_name.size == 0)
-        buffer_name = push_buffer_unique_name(app, scratch, buffer);
-    lister_add_item(lister, buffer_name, status, IntAsPtr(buffer), 0);
+											Buffer_ID buffer){
+	Dirty_State dirty = buffer_get_dirty_state(app, buffer);
+	String_Const_u8 status = {};
+	switch (dirty){
+		case DirtyState_UnsavedChanges:  status = string_u8_litexpr("*"); break;
+		case DirtyState_UnloadedChanges: status = string_u8_litexpr("!"); break;
+		case DirtyState_UnsavedChangesAndUnloadedChanges: status = string_u8_litexpr("*!"); break;
+	}
+	
+	Scratch_Block scratch(app, lister->arena);
+	Variable_Handle prj_var = vars_read_key(vars_get_root(), vars_save_string_lit("prj_config"));
+	String_Const_u8 buffer_name = {};
+	if (!vars_is_nil(prj_var))
+	{
+		String8 prj_dir = prj_path_from_project(scratch, prj_var);
+		buffer_name = push_buffer_file_name(app, scratch, buffer);
+		buffer_name = string_replace(scratch, buffer_name, SCu8("\\"), SCu8("/"), StringFill_NoTerminate);
+		buffer_name = string_replace(scratch, buffer_name, prj_dir, SCu8("./"), StringFill_NoTerminate);
+	}
+	
+	if (buffer_name.size == 0)
+	{
+		buffer_name = push_buffer_unique_name(app, scratch, buffer);
+		buffer_name = string_replace(scratch, buffer_name, SCu8("\\"), SCu8("/"), StringFill_NoTerminate);
+	}
+	lister_add_item(lister, buffer_name, status, IntAsPtr(buffer), 0);
 }
 
 function void
 tc_generate_all_buffers_list(Application_Links *app, Lister *lister){
-    lister_begin_new_item_set(app, lister);
-    
-    Buffer_ID viewed_buffers[16];
-    i32 viewed_buffer_count = 0;
-    
-    // List currently viewed buffers
-    for (View_ID view = get_view_next(app, 0, Access_Always);
-         view != 0;
-         view = get_view_next(app, view, Access_Always)){
-        Buffer_ID new_buffer_id = view_get_buffer(app, view, Access_Always);
-        for (i32 i = 0; i < viewed_buffer_count; i += 1){
-            if (new_buffer_id == viewed_buffers[i]){
-                goto skip0;
-            }
-        }
-        viewed_buffers[viewed_buffer_count++] = new_buffer_id;
-        skip0:;
-    }
-    
-    // Regular Buffers
-    for (Buffer_ID buffer = get_buffer_next(app, 0, Access_Always);
-         buffer != 0;
-         buffer = get_buffer_next(app, buffer, Access_Always)){
-        for (i32 i = 0; i < viewed_buffer_count; i += 1){
-            if (buffer == viewed_buffers[i]){
-                goto skip1;
-            }
-        }
-        if (!buffer_has_name_with_star(app, buffer)){
-            tc_generate_all_buffers_list__output_buffer(app, lister, buffer);
-        }
-        skip1:;
-    }
-    
-    // Buffers Starting with *
-    for (Buffer_ID buffer = get_buffer_next(app, 0, Access_Always);
-         buffer != 0;
-         buffer = get_buffer_next(app, buffer, Access_Always)){
-        for (i32 i = 0; i < viewed_buffer_count; i += 1){
-            if (buffer == viewed_buffers[i]){
-                goto skip2;
-            }
-        }
-        if (buffer_has_name_with_star(app, buffer)){
-            tc_generate_all_buffers_list__output_buffer(app, lister, buffer);
-        }
-        skip2:;
-    }
-    
-    // Buffers That Are Open in Views
-    for (i32 i = 0; i < viewed_buffer_count; i += 1){
-        tc_generate_all_buffers_list__output_buffer(app, lister, viewed_buffers[i]);
-    }
+	lister_begin_new_item_set(app, lister);
+	
+	Buffer_ID viewed_buffers[16];
+	i32 viewed_buffer_count = 0;
+	
+	// List currently viewed buffers
+	for (View_ID view = get_view_next(app, 0, Access_Always);
+		 view != 0;
+		 view = get_view_next(app, view, Access_Always)){
+		Buffer_ID new_buffer_id = view_get_buffer(app, view, Access_Always);
+		for (i32 i = 0; i < viewed_buffer_count; i += 1){
+			if (new_buffer_id == viewed_buffers[i]){
+				goto skip0;
+			}
+		}
+		viewed_buffers[viewed_buffer_count++] = new_buffer_id;
+		skip0:;
+	}
+	
+	Scratch_Block scratch(app, lister->arena);
+	// Project Buffers
+	Variable_Handle prj_var = vars_read_key(vars_get_root(), vars_save_string_lit("prj_config"));
+	if (!vars_is_nil(prj_var))
+	{
+		for (Buffer_ID buffer = get_buffer_next(app, 0, Access_Always);
+			 buffer != 0;
+			 buffer = get_buffer_next(app, buffer, Access_Always)){
+			for (i32 i = 0; i < viewed_buffer_count; i += 1){
+				if (buffer == viewed_buffers[i]){
+					goto skip1;
+				}
+			}
+			if (tc_buffer_is_in_project(app, buffer))
+			{
+				tc_generate_all_buffers_list__output_buffer(app, lister, buffer);
+			}
+			skip1:;
+		}
+	}
+	
+	// Regular Buffers
+	for (Buffer_ID buffer = get_buffer_next(app, 0, Access_Always);
+		 buffer != 0;
+		 buffer = get_buffer_next(app, buffer, Access_Always)){
+		for (i32 i = 0; i < viewed_buffer_count; i += 1){
+			if (buffer == viewed_buffers[i] || tc_buffer_is_in_project(app, buffer)){
+				goto skip2;
+			}
+		}
+		if (!buffer_has_name_with_star(app, buffer)){
+			tc_generate_all_buffers_list__output_buffer(app, lister, buffer);
+		}
+		skip2:;
+	}
+	
+	// Buffers Starting with *
+	for (Buffer_ID buffer = get_buffer_next(app, 0, Access_Always);
+		 buffer != 0;
+		 buffer = get_buffer_next(app, buffer, Access_Always)){
+		for (i32 i = 0; i < viewed_buffer_count; i += 1){
+			if (buffer == viewed_buffers[i]){
+				goto skip3;
+			}
+		}
+		if (buffer_has_name_with_star(app, buffer)){
+			tc_generate_all_buffers_list__output_buffer(app, lister, buffer);
+		}
+		skip3:;
+	}
+	
+	// Buffers That Are Open in Views
+	for (i32 i = 0; i < viewed_buffer_count; i += 1){
+		tc_generate_all_buffers_list__output_buffer(app, lister, viewed_buffers[i]);
+	}
 }
 
 function Buffer_ID
 tc_get_buffer_from_user(Application_Links *app, String_Const_u8 query){
-    Lister_Handlers handlers = lister_get_default_handlers();
-    handlers.refresh = tc_generate_all_buffers_list;
-    Lister_Result l_result = run_lister_with_refresh_handler(app, query, handlers);
-    Buffer_ID result = 0;
-    if (!l_result.canceled){
-        result = (Buffer_ID)(PtrAsInt(l_result.user_data));
-    }
-    return(result);
+	Lister_Handlers handlers = lister_get_default_handlers();
+	handlers.refresh = tc_generate_all_buffers_list;
+	Lister_Result l_result = run_lister_with_refresh_handler(app, query, handlers);
+	Buffer_ID result = 0;
+	if (!l_result.canceled){
+		result = (Buffer_ID)(PtrAsInt(l_result.user_data));
+	}
+	return(result);
 }
 
 function Buffer_ID
 tc_get_buffer_from_user(Application_Links *app, char *query){
-    return(tc_get_buffer_from_user(app, SCu8(query)));
+	return(tc_get_buffer_from_user(app, SCu8(query)));
 }
 
 CUSTOM_UI_COMMAND_SIG(tc_interactive_switch_buffer)
 CUSTOM_DOC("Interactively switch to an open buffer.")
 {
-    Buffer_ID buffer = tc_get_buffer_from_user(app, "Switch:");
-    if (buffer != 0){
-        View_ID view = get_this_ctx_view(app, Access_Always);
-        view_set_buffer(app, view, buffer, 0);
-    }
+	Buffer_ID buffer = tc_get_buffer_from_user(app, "Switch:");
+	if (buffer != 0){
+		View_ID view = get_this_ctx_view(app, Access_Always);
+		view_set_buffer(app, view, buffer, 0);
+	}
 }
